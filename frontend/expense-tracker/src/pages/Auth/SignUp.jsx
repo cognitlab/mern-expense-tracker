8267dd5a8afb118bdda232479e85bd029e2d6c8a
cognitlab <dwarkeshthanki738@gmail.com>
2025-09-04 00:50:20 +0530

    ✨ feat(auth): complete signup with profile image upload 🖼️✅.

--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,15 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import AuthLayout from "../../components/layouts/AuthLayout";
 import {useNavigate, Link} from 'react-router-dom';
 import Input from "../../components/layouts/Inputs/Input"
 import { validateEmail } from "../../utils/helper";
 import ProfilePhotoSelector from '../../components/layouts/Inputs/ProfilePhotoSelector';
+import { UserContext } from '../../context/userContext';
+import uploadImage from '../../utils/uploadImage';
+import axiosInstance from "../../utils/axiosInstance"; 
+import { API_PATHS } from "../../utils/apiPaths";
+
+
 
 const SignUp = () => {
   const [profilePic, setProfilePic] = useState(null);
@@ -12,6 +18,8 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
 
   const [error, setError] = useState(null);
+
+  const {updateUser} = useContext(UserContext)
 
   const navigate = useNavigate();
 
@@ -38,6 +46,37 @@ const SignUp = () => {
 
 
     //SignUp API call
+    try {
+
+      // Upload image if present
+      if (profilePic) {
+        const imgUploadRes = await uploadImage(profilePic);
+        profileImageUrl = imgUploadRes.imageUrl || "";
+      }
+
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        fullName,
+        email,
+        password,
+        profileImageUrl
+      });
+
+      const { token, user } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(user);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } 
+      else{
+        setError("Something went wrong. Please try again.", error);
+      }
+    }
   };
 
 
