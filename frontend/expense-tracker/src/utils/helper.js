493c6d38670abcc2ebdc97656955b54fee802b1f
cognitlab <dwarkeshthanki738@gmail.com>
2025-09-08 00:54:15 +0530

    feat: Completed SideMenu, added CharAvatar, integrated user auth 🚀
    - Finalized SideMenu UI & functionality.
    - Added CharAvatar feature for user initials.
    - Integrated user authentication into Dashboard layout.

--- frontend/expense-tracker/src/utils/helper.js
+++ frontend/expense-tracker/src/utils/helper.js
@@ -2,3 +2,15 @@ export const validateEmail = (email) => {
   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return re.test(String(email).toLowerCase());
 };
+
+export const getInitials = (name) => {
+  if (!name) return "";
+
+  const words = name.split(" ");
+  let intitials = "";
+
+  for (let i = 0; i < Math.min(words.length, 2); i++) {
+    intitials += words[i][0];
+  }
+  return intitials.toUpperCase();
+};
